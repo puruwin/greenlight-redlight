aac676769e348cf422bb04f3d0049303bdbf97d5
puruwin <taborlindp@gmail.com>
2023-12-28 20:09:46 +0100

    added randomness to the trafficlights! 🎲

--- src/Game.elm
+++ src/Game.elm
@@ -4,7 +4,7 @@ import Html exposing (Html, div, p, img, text)
 import Browser
 import Html.Attributes exposing (class, src, width)
 import Html.Events exposing (onClick)
-import Time
+import Random exposing (int)
 import Process
 import Task
 
@@ -16,6 +16,7 @@ type alias Model =
   , highScore : Int
   , remainingLives : Int
   , greenLight : Bool
+  , randomSeed : Int
   }
 
 initialModel : Model
@@ -24,13 +25,15 @@ initialModel =
   , score = 0
   , highScore = 0
   , remainingLives = 3
-  , greenLight = True
+  , greenLight = False
+  , randomSeed = 0
   }
 
 -- Update
 type Msg
   = ClickedStep
   | SwitchLights
+  | NewSeed Int
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
@@ -38,8 +41,16 @@ update msg model =
   case msg of
     ClickedStep ->
       ( { model | score = model.score + 1 }, Cmd.none )
+
     SwitchLights ->
-      ( model, changeTrafficLights ( { model | greenLight = not model.greenLight } ) )
+      let
+        updatedModel = { model | greenLight = not model.greenLight }
+      in
+        ( updatedModel, changeTrafficLights updatedModel )
+
+    NewSeed seed ->
+      ( { model | randomSeed = seed }, Cmd.none )
+
 
 -- View
 view : Model -> Html Msg
@@ -75,15 +86,21 @@ view model =
   ]
   ]
 
-
 changeTrafficLights : Model -> Cmd Msg
 changeTrafficLights model =
     if model.greenLight then
-        Process.sleep 3000
+        let
+            randomNumber = round ((max (10000 - (toFloat model.score * 100)) 2000) + (toFloat model.randomSeed))
+        in
+        Process.sleep (toFloat randomNumber)
         |> Task.perform ( \_ -> SwitchLights )
     else
-        Process.sleep 6000 
+        Process.sleep 3000 
         |> Task.perform ( \_ -> SwitchLights )
+
+newSeed : Cmd Msg
+newSeed =
+    Random.generate NewSeed (Random.int -750 750)
 
 init : () -> (Model, Cmd Msg)
 init _ =
